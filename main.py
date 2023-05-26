6c04d3bccde856c6e3a09eeec2d5967ed963a9f8
npapera <ngere.papera@students.mq.edu.au>
2023-05-27 00:34:03 +1000

    fixed error handling and jsonItem building

--- main.py
+++ main.py
@@ -10,18 +10,22 @@ from utils.json_writer import make_json
 load_dotenv()
 DEVELOPER_KEY = os.getenv("API_KEY")
 minIndex = 0
-maxIndex = 500
+maxIndex = 2992
 totalIndex = maxIndex - minIndex
 batchId = 'B1-SS1'
-batchFile = f'{batchId}_DT{totalIndex}_{minIndex}-{maxIndex}'
-outputDir = f'analysis/PANS_{batchFile}'
+batchFile = f'PANS_{batchId}_DT{totalIndex}_{minIndex}-{maxIndex}'
+outputDir = f'analysis/{batchFile}'
 
 logging.basicConfig(filename=f'tmp/{batchFile}.log',
                     level=logging.DEBUG,
                     format='%(asctime)s %(levelname)s %(name)s %(message)s')
 logger=logging.getLogger(__name__)
 
-def commentAnalyser(text):
+def commentAnalyser(text, cansIndex):
+
+    jsonItem = {}
+    jsonItem["textOriginal"] = text
+    jsonItem["Index"] = cansIndex
 
     try:
         client = discovery.build(
@@ -45,37 +49,36 @@ def commentAnalyser(text):
         }
 
         response = client.comments().analyze(body=analyze_request).execute()
+
+        holdThis = response
+        toxicScore = holdThis['attributeScores']['TOXICITY']['summaryScore']['value']
+        sevToxicScore = holdThis['attributeScores']['SEVERE_TOXICITY']['summaryScore']['value']
+        idAttackScore = holdThis['attributeScores']['IDENTITY_ATTACK']['summaryScore']['value']
+        insultScore = holdThis['attributeScores']['INSULT']['summaryScore']['value']
+        profanityScore = holdThis['attributeScores']['PROFANITY']['summaryScore']['value']
+        threatScore = holdThis['attributeScores']['THREAT']['summaryScore']['value']
+
+        jsonItem["TOXICITY"] = toxicScore
+        jsonItem["SEVERE_TOXICITY"] = sevToxicScore
+        jsonItem["TOXICITY"] = idAttackScore
+        jsonItem["INSULT"] = insultScore
+        jsonItem["PROFANITY"] = profanityScore
+        jsonItem["THREAT"] = threatScore
+
     except Exception as e:
         print(f"Unexpected Error: {e}")
         logger.error(e)
 
-    holdThis = response
-    toxicScore = holdThis['attributeScores']['TOXICITY']['summaryScore']['value']
-    sevToxicScore = holdThis['attributeScores']['SEVERE_TOXICITY']['summaryScore']['value']
-    idAttackScore = holdThis['attributeScores']['IDENTITY_ATTACK']['summaryScore']['value']
-    insultScore = holdThis['attributeScores']['INSULT']['summaryScore']['value']
-    profanityScore = holdThis['attributeScores']['PROFANITY']['summaryScore']['value']
-    threatScore = holdThis['attributeScores']['THREAT']['summaryScore']['value']
-
-    jsonItem = {}
-    jsonItem["textOriginal"] = text
-    jsonItem["TOXICITY"] = toxicScore
-    jsonItem["SEVERE_TOXICITY"] = sevToxicScore
-    jsonItem["TOXICITY"] = idAttackScore
-    jsonItem["INSULT"] = insultScore
-    jsonItem["PROFANITY"] = profanityScore
-    jsonItem["THREAT"] = threatScore
-
     return jsonItem
 
 def main():
-    to_json = True
+    to_json = False
     file = open('data/analysis/CANS_B1-SS1_DT100_2250_24-05-2023.json')
     data = json.load(file)
     all_data = []
     for i in range(minIndex, maxIndex):
         logger.info(f"Processing comment at Index: {i}")
-        all_data.append(commentAnalyser(data[i]['textOriginal']))
+        all_data.append(commentAnalyser(data[i]['textOriginal'], i))
         time.sleep(1)
         print(f"Data analysed by perspective at Index: {i}")
         logger.info(f"Finished analysing comment at Index: {i}")
