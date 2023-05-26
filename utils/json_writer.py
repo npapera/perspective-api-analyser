e510ceb88e62a637b4ae47960d714efbbf11575d
npapera <ngere.papera@students.mq.edu.au>
2023-05-27 00:34:16 +1000

    edited filename

--- utils/json_writer.py
+++ utils/json_writer.py
@@ -6,7 +6,7 @@ today = dt.today().strftime('%H%M_%d-%m-%Y')
 def make_json(input, output):
     json_formatted_str = json.dumps(input, indent=2)
 
-    filename = f'data/{output}{today}.json'
+    filename = f'data/{output}_{today}.json'
     
     with open(filename, "w") as outfile:
         outfile.write(json_formatted_str)
